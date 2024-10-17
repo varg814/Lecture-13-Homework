08c6ba73e31792b87912e1a4547513b2eb21768f
giorgi <ge22360017@eu.edu.ge>
2024-10-17 21:33:12 +0400

    lecture-13-homework

---
+++ app.js
@@ -0,0 +1,12 @@
+const name = "giorgi";
+const surname = "varsimashvili";
+let age = 19;
+const email = "ge22360017@eu.edu.ge";
+const tel = "597 09 83 53";
+const isOfLegalAge = true;
+console.log(name)
+console.log(surname)
+console.log(age)
+console.log(email)
+console.log(`%c ${tel}`, 'color:red;')
+console.log(`%c ${isOfLegalAge}`, 'color:yellow;')
